7602492c62af2b51627c10b5b906f02e1a848ab1
larrisdev <mcacerez00@gmail.com>
2026-05-08 22:38:12 -0600

    fix: pantalla de inicio y controlador Home

---
+++ Portal_Web_Colegio/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Portal_Web_Colegio.Models;
+using System.Diagnostics;
+
+namespace Portal_Web_Colegio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
